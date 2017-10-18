13122b4cccc331de087de7afb572153d0c09a3a9
andrew-PC\andrew <andrew@riko.com.tw>
2017-10-18 22:48:55 +0800

    練習 todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// use FormsModule for [(ngModel)]
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ToDoInputComponent } from './todoInput/todoInput.component';
+import { ToDoListComponent } from './todoList/todoList.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent , ToDoInputComponent , ToDoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
